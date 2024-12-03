a4cf1657f41f42342ad0be733ce117d9a0556996
Robin <97387960+robeecodes@users.noreply.github.com>
2024-12-03 12:41:55 +0000

    fix light flicker randomisation

--- refresher/Assets/Scripts/LightOnOff.cs
+++ refresher/Assets/Scripts/LightOnOff.cs
@@ -4,13 +4,16 @@ using Random = UnityEngine.Random;
 
 public class LightOnOff : MonoBehaviour
 {
+    private Light _light;
+    
     private void Start() {
+        _light = gameObject.GetComponent<Light>();
         int on = Random.Range(0, 10);
 
-        gameObject.SetActive(on <= 5);
+        _light.enabled = on > 5 ? true : false;
     }
 
     public void Flicker() {
-        gameObject.SetActive(!gameObject.activeSelf);
+        _light.enabled = !_light.enabled;
     }
 }
